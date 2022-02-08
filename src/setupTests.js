328b41b18766f0f2c43d05ef53b0299eac4ab3fa
coderylicode <rylidavis94@gmail.com>
2022-02-08 13:44:43 -0800

    imports in app.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,12 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/*We started out by creating a react app
+using npx create react app. Then we did git init
+in order to initalize repo. Then we are downloading
+dependencies we will need. First one is:
+npm install react-router-dom react-icons --save uuid 
+Next we deleted app.css and we deleted what was in the react div in app.js. we imported  { BrowserRouter, Route, Switch } from "react-router-dom"; Then we added browserrouter tags in our return statement so it would return h1 element of our name. */
+
+
+
+
+
 import '@testing-library/jest-dom';
