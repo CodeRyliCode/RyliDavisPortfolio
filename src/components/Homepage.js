bddee3041c71612e6d999e7c1333d052c4660cc5
coderylicode <rylidavis94@gmail.com>
2022-02-15 23:06:18 -0800

    Contact Links

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,17 +1,39 @@
+import {FaGithub, FaLinkedin, FaLinkedinIn} from "react-icons/fa"
 const Homepage = () => {
   return (
-      <>
-<section> 
-   <h1>Ryli Davis</h1> 
-   <p>Front-end Web Developer</p>
+		<>
+			<section className="homepage">
+				<div className="overlay">
+					<h1>Ryli Davis</h1>
+					<p>Front-end Web Developer</p>
 
-<ul>
-   <li><a href ="https://twitter.com"></a></li>
-</ul>
-
-    </section>
-    </>
-   )
+					<ul>
+						<li>
+							<a
+								href="https://github.com/CodeRyliCode/"
+								target="_blank"
+								rel="noopenner noreferrer"
+							>
+							{/* this code will make the link open in a new tab: target="_blank" rel="noopenner noreferrer */}
+								{"Github"}
+								<FaGithub />
+							</a>
+						</li>
+						<li>
+							<a
+								href="https://www.linkedin.com/in/ryli-davis-b48354130/"
+								target="_blank"
+								rel="noopenner noreferrer"
+							>
+								{"LinkedIn"}
+								<FaLinkedin />
+							</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default Homepage;
