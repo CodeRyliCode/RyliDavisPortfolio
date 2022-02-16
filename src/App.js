ec06aa79d445ea3f79d88df0cbfb93a91b432bef
coderylicode <rylidavis94@gmail.com>
2022-02-15 22:31:50 -0800

    Styling error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 
 //Components
 import Homepage from "./components/Homepage"
+import Error from "./pages/Error";
 
 // Pages
 import Projects from "./pages/Projects";
@@ -10,12 +11,13 @@ import Projects from "./pages/Projects";
 
 const App = () => {
   return (
-      <Routes>
-     <Route path="/" element={<Homepage />}></Route>
-  <Route path="/projects" element={<Projects />}></Route>
-  </Routes>
-
-  )
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="*" element={<Error />} />
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+		<div>
+			<h1 style={{ fontSize: 200, textAlign: "center" }}>404</h1>
+			<p style={{ fontSize: 30, textAlign: "center" }}> Page Not Found</p>
+		</div>
+	);
+}
+
+export default Error
