a363556762d246cd18d7819373b7250f25829912
coderylicode <rylidavis94@gmail.com>
2022-02-19 17:59:08 -0800

    css styling all around

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,18 +2,22 @@ import {FaPhone, FaEnvelope} from "react-icons/fa"
 const Contacts = () => {
   return (
 		<>
-
 			<section className="contacts">
-							<h1>Contact Me</h1>
+				<h1>Contact Me</h1>
 
-				<ul>
-					<li>
-						<a href="tel:2099868880"><FaPhone/>(209) 986-8880</a>
-					</li>
-					<li>
-						<a href="mailto:rylidavis94@gmail.com"><FaEnvelope />{" "}rylidavis94@gmail.com</a>
-					</li>
-				</ul>
+					<ul>
+						<li>
+							<a href="tel:2099868880">
+								<FaPhone />
+								(209) 986-8880
+							</a>
+						</li>
+						<li>
+							<a href="mailto:rylidavis94@gmail.com">
+								<FaEnvelope /> rylidavis94@gmail.com
+							</a>
+						</li>
+					</ul>
 			</section>
 		</>
 	);
